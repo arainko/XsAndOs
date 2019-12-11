740cf63d8c43c000075e0cbd3411a97f5c7428ff
Aleksander Rainko <aleksander.rainko99@gmail.com>
2019-12-11 21:50:40 +0100

    color corrections for buttons and background

--- src/com/arainko/xno/ui/Main.java
+++ src/com/arainko/xno/ui/Main.java
@@ -3,6 +3,8 @@ package com.arainko.xno.ui;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +13,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Game Board");
         ButtonBoard buttonBoard = new ButtonBoard(10,10);
-
+        buttonBoard.setBackground(Background.EMPTY);
         Scene scene = new Scene(buttonBoard, 800, 600);
+        scene.setFill(Color.rgb(66,66,66));
         scene.getStylesheets().add("style.css");
         buttonBoard.setAlignment(Pos.CENTER);
         primaryStage.setScene(scene);
