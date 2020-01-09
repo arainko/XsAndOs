05a47163aa1af95b3d228855c28126961a93cd79
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-01-09 17:52:20 +0100

    Added tests

--- src/com/arainko/tests/ModelTests.java
+++ src/com/arainko/tests/ModelTests.java
@@ -1,6 +1,5 @@
 package com.arainko.tests;
 
-import com.arainko.xno.abstracts.Board;
 import com.arainko.xno.model.board.ModelBoard;
 import com.arainko.xno.model.elements.Cell;
 import com.arainko.xno.model.elements.Connection;
@@ -14,8 +13,7 @@ import static com.arainko.xno.model.predicates.BoardPredicates.ableToAccommodate
 import static com.arainko.xno.model.predicates.BoardPredicates.done;
 import static com.arainko.xno.model.predicates.CellPredicates.*;
 import static com.arainko.xno.model.predicates.ConnectionPredicates.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ModelTests {
 
@@ -63,22 +61,33 @@ public class ModelTests {
     }
 
     @Test
-    public void cellPredicatesTest() {
+    public void cellContentsTests() {
         Cell cell1 = new Cell(0,0, Cell.Contents.EMPTY);
-
         assertFalse(cell1.isCell(containingCircle()));
         assertFalse(cell1.isCell(containingCross()));
         cell1.setCellContents(Cell.Contents.CIRCLE);
         assertTrue(cell1.isCell(containingCircle()));
         assertFalse(cell1.isCell(containingCross()));
+    }
 
+    @Test
+    public void cellConnectionFlagTest() {
+        Cell cell1 = new Cell(0,0, Cell.Contents.EMPTY);
         assertFalse(cell1.isCell(partOfConnection()));
         exampleConnection.addConnectionUnit(cell1);
         assertTrue(cell1.isCell(partOfConnection()));
+    }
 
+    @Test
+    public void cellConnectionJointDetectionTest() {
+        Cell cell1 = new Cell(0,0, Cell.Contents.EMPTY);
         assertTrue(cell1.isCell(connectedByJoint(), new Cell(1,1, Cell.Contents.EMPTY)));
         assertFalse(cell1.isCell(connectedByJoint(), new Cell(1,0, Cell.Contents.EMPTY)));
+    }
 
+    @Test
+    public void cellPlaneCalculationTest() {
+        Cell cell1 = new Cell(0,0, Cell.Contents.EMPTY);
         assertTrue(cell1.isCell(nextToOnPlaneX(), new Cell(1,0, Cell.Contents.EMPTY)));
         assertTrue(cell1.isCell(nextToOnPlaneY(), new Cell(0,1, Cell.Contents.EMPTY)));
         assertFalse(cell1.isCell(nextToOnPlaneY(), new Cell(1,1, Cell.Contents.EMPTY)));
@@ -86,21 +95,59 @@ public class ModelTests {
     }
 
     @Test
-    public void boardPredicatesTest() {
+    public void boardCompletenessTest() {
+        mockConnection();
+        assertFalse(modelBoard.isBoard(done()));
+        modelBoard.addConnection(exampleConnection);
+        assertTrue(modelBoard.isBoard(done()));
+    }
+
+    @Test
+    public void boardCordsAccommodationTest() {
+        assertFalse(modelBoard.isBoard(ableToAccommodateCords(-1, 10)));
+        assertFalse(modelBoard.isBoard(ableToAccommodateCords(5, 20)));
+        assertTrue(modelBoard.isBoard(ableToAccommodateCords(5, 5)));
+    }
+
+    @Test
+    public void boardConnectionAdditionTest() {
+        mockConnection();
+        modelBoard.addConnection(exampleConnection);
+        assertFalse(modelBoard.getConnections().isEmpty());
+    }
+
+    @Test
+    public void boardConnectionRemovalTest() {
+        mockConnection();
+        modelBoard.addConnection(exampleConnection);
+        assertFalse(modelBoard.getConnections().isEmpty());
+        modelBoard.removeConnection(exampleConnection);
+        assertTrue(modelBoard.getConnections().isEmpty());
+    }
+
+    @Test
+    public void boardSpecificConnectionGetterTest() {
+        mockConnection();
+        modelBoard.addConnection(exampleConnection);
+        assertSame(exampleConnection, modelBoard
+                .getSpecificConnection(containingCell(new Cell(1, 0, Cell.Contents.EMPTY))));
+    }
+
+    @Test
+    public void boardNeighborGetterTest() {
+        mockConnection();
+        modelBoard.addConnection(exampleConnection);
+        assertEquals(2, modelBoard.getFreeNeighborsAt(new Cords(1, 1)).size());
+        assertEquals(4, modelBoard.getFreeNeighborsAt(new Cords(5, 5)).size());
+        assertEquals(2, modelBoard.getFreeNeighborsAt(new Cords(9, 9)).size());
+    }
+
+    private void mockConnection() {
         List<Cords> cordList = List.of(new Cords(0,0), new Cords(1,0), new Cords(2,0),
                 new Cords(2,1), new Cords(2,2));
         modelBoard.getElementAt(new Cords(0,0)).setCellContents(Cell.Contents.CIRCLE);
         modelBoard.getElementAt(new Cords(2,2)).setCellContents(Cell.Contents.CROSS);
         List<Cell> connectionCells = modelBoard.getElementsAt(cordList);
         exampleConnection = new Connection(connectionCells);
-
-        assertFalse(modelBoard.isBoard(ableToAccommodateCords(-1, 10)));
-        assertFalse(modelBoard.isBoard(ableToAccommodateCords(5, 20)));
-        assertTrue(modelBoard.isBoard(ableToAccommodateCords(5, 5)));
-
-        assertFalse(modelBoard.isBoard(done()));
-        modelBoard.addConnection(exampleConnection);
-        assertTrue(modelBoard.isBoard(done()));
     }
-
 }
